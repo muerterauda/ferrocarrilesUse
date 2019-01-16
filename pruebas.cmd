426fdc5e57b75e7677f60c5a57dc835a5de8a8d2
muerterauda <juanpalmaborda@uma.es>
2019-01-16 17:09:03 +0100

    CMD

--- pruebas.cmd
+++ pruebas.cmd
@@ -1,18 +1,34 @@
 !new Tren('Tr1');
+!Tr1.posicion:='Antequera';
 !new Tren('Tr2');
+!Tr2.posicion:='Via1';
 !new Tren('Tr3');
+!Tr3.posicion:='Via1';
 
 !new Estacion('E1');
+!E1.nTrenes:=1
+!E1.nombre:='Antequera';
 !new Estacion('E2');
+!E2.nTrenes:=1
+!E2.nombre:='Pizarra';
 
 !new Tramo('T1');
+!T1.nTrenes:=1
 !new Tramo('T2');
+!T2.nTrenes:=0
 !new Tramo('T10');
+!T10.nTrenes:=1
 !new Tramo('T11');
+!T11.nTrenes:=0
 !new Tramo('T12');
+!T12.nTrenes:=0
 
 !new Via('V1');
+!V1.nTrenes:=0
+!V1.nombre:='Via3'
 !new Via('V2');
+!V2.nTrenes:=2
+!V2.nombre:='Via1'
 
 !insert (Tr3,E1) into ComponenteTren;
 !insert (Tr2,T1) into ComponenteTren;
